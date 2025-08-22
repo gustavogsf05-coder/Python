85b06471a44d7cb70a7231721e3e21e27ce705f8
gustavogsf05-coder <gustavogsf05@gmail.com>
2025-08-21 22:37:49 -0300

    terceiro

---
+++ Python_algoritimo/Exercicio3.py
@@ -0,0 +1,5 @@
+dias = int(input("digite a quantidade de dias: "))
+h = int(input("digite a quantidade de horas: "))
+min = int(input("digite a quantidade de minutos: "))
+sec = int(input("digite a quantidade de segundos: "))
+
