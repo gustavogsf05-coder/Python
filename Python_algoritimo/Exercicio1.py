3333127f50d50357c804d27b5439808bad17cbf0
gustavogsf05-coder <gustavogsf05@gmail.com>
2025-08-26 21:30:53 -0300

    Exercicios aula2

---
+++ Python_algoritimo/Exercicio1.py
@@ -0,0 +1,6 @@
+altura = float(input("olá. qual é o sua altura atual?: "))
+peso = float(input("agora pode digite o seu peso atual: "))
+1.86
+pesoi = (72.7*altura)-58
+
+print(f"seu peso idela é {pesoi:.1f}")
---
+++ Python_algoritimo/Exercicio4.py
@@ -0,0 +1,16 @@
+salario = float(input("digite a quanto você ganha por hora: "))
+horas= int(input("digite as horas trabalhadas no mês: "))
+
+
+salario = salario * horas
+imrenda = salario *0.11
+inss =  salario *0.08   
+sindicato = salario *0.05
+
+sliquido = salario - imrenda - inss - sindicato
+
+print(f"-salario bruto: {salario}")
+print(f"-imposto de renda: {imrenda}")
+print(f"-inss: {inss}")
+print(f"-sindicato: {sindicato}")
+print(f"-salario liquido: {sliquido}")
