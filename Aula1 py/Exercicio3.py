3e547a3d2545e7105857bea88c37b87bd7780d34
gustavogsf05-coder <gustavogsf05@gmail.com>
2025-09-05 16:19:14 -0300

    Delete Aula1 py directory

--- Aula1 py/Exercicio3.py
+++
@@ -1,8 +0,0 @@
-dias = int(input("digite a quantidade de dias: "))
-h = int(input("digite a quantidade de horas: "))
-min = int(input("digite a quantidade de minutos: "))
-sec = int(input("digite a quantidade de segundos: "))
-
-totalsec = (dias * 86400) + (h * 3600) + (min*60) + sec
-
-print(f"o total de segundos foi: {totalsec:.2f}")
