50502aab46e8701caad7673aa581dee2485ca658
gustavogsf05-coder <gustavogsf05@gmail.com>
2025-09-05 16:19:03 -0300

    Delete Python_algoritimo directory

--- Python_algoritimo/AulaPratica1.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input("Digite um numero: "))
-n2 = int(input("Digite outro numero:"))
-soma = n1 + n2
-print("Soma dos numeros: %d" % soma)
--- Python_algoritimo/Exercicio2.py
+++
@@ -1,5 +0,0 @@
-km = float(input("digite a quantidade de quilometros percorridos: "))
-dias = int(input("digite a quantidade de dias: "))
-valcobra = (km *0.15) + (dias * 60)
-
-print(f"Total a pagar: {valcobra:.1f}")
